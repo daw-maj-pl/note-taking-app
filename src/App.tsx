bd851a950dd22a3edbc748209e9c1450b1016631
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2023-02-20 19:31:04 +0100

    Set up Bootstrap and React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>NoteList</h1>} />
+        <Route path="/new" element={<h1>NewNote</h1>} />
+        <Route path="/:id">
+          <Route index element={<h1>Note</h1>} />
+          <Route path="edit" element={<h1>EditNote</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
